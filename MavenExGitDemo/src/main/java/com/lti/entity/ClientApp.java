a841f5c28115423410fb305f83394f5beada4227
Atharva Hankare <69903865+atharva009@users.noreply.github.com>
2022-07-14 12:52:41 +0530

    Create ClientApp.java

---
+++ MavenExGitDemo/src/main/java/com/lti/entity/ClientApp.java
@@ -0,0 +1,14 @@
+package com.lti.entity;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome Client!");
+		
+		Calculator c = new Calculator();
+		System.out.println(c.add(50, 50));
+
+	}
+
+}
