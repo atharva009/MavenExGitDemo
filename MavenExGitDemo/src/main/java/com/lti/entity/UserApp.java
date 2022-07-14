903b4e7abe83f1623b0d0cf4bf5d00752bf7bff1
atharva009 <atharvahankare@gmail.com>
2022-07-14 12:34:38 +0530

    Third Commit

--- MavenExGitDemo/src/main/java/com/lti/entity/UserApp.java
+++ MavenExGitDemo/src/main/java/com/lti/entity/UserApp.java
@@ -4,7 +4,7 @@ public class UserApp {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome");
+		System.out.println("Welcome User!");
 		
 		Calculator c = new Calculator();
 		System.out.println(c.add(10, 20));
